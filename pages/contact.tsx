a63183e3d0c4f6cd1b82a67fc7ec3bdc4a2032ec
Tony Vu <tonyvu2008@gmail.com>
2024-07-14 23:51:06 +1000

    update form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,8 +22,8 @@ const Contact: NextPage = (props: any) => {
         <Header />
         <BaseBox>
             <Container maxW="container.md" m="auto" mt="4" centerContent>
-            <Heading as="h1" mb="4">Registration Form</Heading>         
-            <FormfacadeEmbed
+              <Heading as="h1" mb="4">Registration Form</Heading>         
+              <FormfacadeEmbed
                 formFacadeURL="https://formfacade.com/include/110581543394745721401/form/1FAIpQLSfsrThogKVdPBRsCUJJ6meIXM9jCdGCYLRsIdq5D7oF-A_ufg/classic.js/?div=ff-compose"
                 onSubmitForm={() => console.log('Form submitted')}
                 />

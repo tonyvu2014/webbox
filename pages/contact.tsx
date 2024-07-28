83106007393761f7114dceb54e756b104791eca2
Tony Vu <tonyvu2008@gmail.com>
2024-07-28 16:29:32 +1000

    change sign up title

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -16,13 +16,13 @@ import BaseBox from "@/modules/components/BaseBox";
 const Contact: NextPage = (props: any) => {
   return (
     <div>
-      <BaseSeo title='Registration Form'/>
+      <BaseSeo title='Sign Up for Early Access'/>
 
       <main>
         <Header />
         <BaseBox>
             <Container maxW="container.md" m="auto" mt="8" centerContent>
-              <Heading as="h1" mb="4">Registration Form</Heading>         
+              <Heading as="h1" mb="4">Sign Up for Early Access</Heading>         
               <FormfacadeEmbed
                 formFacadeURL="https://formfacade.com/include/110581543394745721401/form/1FAIpQLSfsrThogKVdPBRsCUJJ6meIXM9jCdGCYLRsIdq5D7oF-A_ufg/classic.js/?div=ff-compose"
                 onSubmitForm={() => console.log('Form submitted')}
